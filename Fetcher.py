01112a2a3272a9cf6004237aeb7ee32f22c5f11f
unknown <huangxiaojun@DC3E4572896DEFE.internal.baidu.com>
2015-04-30 14:23:22 +0800

    'Init'

---
+++ Fetcher.py
@@ -0,0 +1,219 @@
+#! /usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+    Author:Xiaojun Huang
+    Date:2015-03-28-15:20
+    Version:0.1.0
+    利用cookie模拟登录手机版新浪微博，爬取某个用户的用户信息(粉丝数、微博数、关注数、标签)
+'''
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import cookielib
+import urllib2
+import urllib
+import lxml.html as HTML
+import re
+import os
+import time
+
+
+class Fetcher(object):
+    def __init__(self,username,pwd,cookie_filename=None):
+        self.cj = cookielib.LWPCookieJar()
+        if cookie_filename is not None:
+            self.cj.load(cookie_filename)
+        self.cookie_processor = urllib2.HTTPCookieProcessor(self.cj)
+        self.opener = urllib2.build_opener(self.cookie_processor, urllib2.HTTPHandler)
+        urllib2.install_opener(self.opener)
+
+        self.username = username
+        self.pwd = pwd
+        #self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:14.0) Gecko/20100101 Firefox/14.0.1',
+        #               'Referer':'','Content-Type':'application/x-www-form-urlencoded'}
+        self.headers = {  
+            #'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0',
+            'Referer':'http://login.weibo.cn/login/' ,
+            'Content-Type':'application/x-www-form-urlencoded'
+        }  
+
+    def get_rand(self, url):
+        #headers = {'User-Agent':'Mozilla/5.0 (Windows;U;Windows NT 5.1;zh-CN;rv:1.9.2.9)Gecko/20100824 Firefox/3.6.9',
+        #           'Referer':''}
+        req = urllib2.Request(url ,urllib.urlencode({}), self.headers)
+        resp = urllib2.urlopen(req)
+        login_page = resp.read()
+        rand = HTML.fromstring(login_page).xpath("//form/@action")[0]
+        passwd = HTML.fromstring(login_page).xpath("//input[@type='password']/@name")[0]
+        vk = HTML.fromstring(login_page).xpath("//input[@name='vk']/@value")[0]
+        return rand, passwd, vk
+
+    def login(self):
+        url = 'http://login.weibo.cn/login/' #登录页面url
+        #url = 'https://passport.weibo.cn/signin/login'
+        rand, passwd, vk = self.get_rand(url)
+        data = urllib.urlencode({'mobile': self.username,
+                                    passwd: self.pwd,
+                                    'remember': 'on',
+                                    'backURL': 'http://weibo.cn/',
+                                    'backTitle': '新浪微博',
+                                    'vk': vk,
+                                    'submit': '登录',
+                                    'encoding': 'utf-8'})
+        req = urllib2.Request(url,data,self.headers)
+        resq = urllib2.urlopen(req)
+        page = resq.read()
+        HTML_str = HTML.fromstring(page)
+        divs = HTML_str.xpath("//div[@class='tip2']//a")
+        if not len(divs):
+            raise LoginError("Login failed!")
+        else:
+            print "Login successfully!"
+
+    def openURL(self,url,type=1):
+        '''
+            打开url所指向的页面，
+            如果type=1，则返回该页面的HtmlElement对象
+            如果type=0，怎返回该页面的str对象
+        '''
+        req = urllib2.Request(url,urllib.urlencode({}),self.headers)
+        page = urllib2.urlopen(req).read().encode('utf-8')
+        if type == 0:
+            return page
+        else:
+            return HTML.fromstring(page)
+
+    def get_user_weibos (self,uid,topk = 250):
+        '''
+            获得用户最近发布的topk条微博，默认为最近的250条
+            返回微博列表
+        '''
+        if not isinstance(uid,unicode):
+            raise ValueError('Type of uid must be unicode!')
+        outputs = []
+        page = 1
+        while len(outputs) <= topk and page < 30:
+            home_url = 'http://weibo.cn/u/' + str(uid) + "?page=" + str(page)
+            HTML_str = self.openURL(home_url,0)
+            time.sleep(0.5)
+            if page == 1:
+                weibos = re.findall(r'<span class="ctt">.*?</span>',HTML_str)[3:]
+            else: 
+                weibos = re.findall(r'<span class="ctt">.*?</span>',HTML_str)
+
+            for weibo in weibos:
+                weibo = re.sub(r'<.*?>','',weibo)
+                outputs.append(weibo)
+            page += 1
+        if len(outputs) < topk:
+             return outputs
+        else:
+            return outputs[0:topk]
+
+    def get_user_follows(self,uid):
+        '''
+            input:uid,unicode
+            output:follows_list,用户的关注列表
+        '''
+        follows_list = []
+        page = 1
+        while page <= 15:
+            follows_page = 'http://weibo.cn/' + str(uid) + "/follow?page=" + str(page)
+            HTML_str = self.openURL(follows_page)
+            time.sleep(0.5)
+            follows = HTML_str.xpath('//td[@ valign="top"]/a')
+            #print len(follows)
+            for item in follows:
+                if item.text == '关注他':
+                    follow_uid = re.findall(r'uid=\d+',item.attrib['href'])[0]
+                    follow_uid = re.sub(r'uid=','',follow_uid)
+                    follows_list.append(follow_uid)
+                    #print follow_uid
+            page += 1
+        return follows_list
+
+    def get_user_tags(self,uid):
+        '''
+            获得用户的标签信息
+            input：
+                用户uid,unicode
+            output：
+                info_dict:返回用户信息字典
+                {'uid':用户的uid,tags':用户标签(每个标签以,作为分割符),'profile':用户简介信息}
+        '''
+        if not isinstance(uid,unicode):
+            raise ValueError('Type of uid must be unicode!')
+        info_dict = {}
+        info_dict['uid'] = uid
+
+        try:
+            #抓取用户标签
+            tags_url = 'http://weibo.cn/account/privacy/tags/?uid=' + str(uid)#标签页url
+            HTML_str = self.openURL(tags_url)
+            tags_words = HTML_str.xpath("//div[@class='c']//a")
+            tags_filter_words = [u'皮肤',u'图片',u'条数',u'隐私',u'触屏',u'语音']
+            tags_list = []
+            for key,tag in enumerate(tags_words):
+                if key:
+                    if tag.text not in tags_filter_words:
+                        tags_list.append(tag.text)
+            info_dict['tags'] = ",".join(tags_list)
+
+            info_url = 'http://weibo.cn/' + str(uid) + '/info'
+            req = urllib2.Request(info_url,urllib.urlencode({}),self.headers)
+            page = urllib2.urlopen(req).read().encode('utf-8')
+            info_list = re.findall(r'昵称.*<br/>',page)
+            if len(info_list):
+                info_words = info_list[0].split('<br/>')
+            filter_words = ['认证:','认证信息：']
+            other_info = []
+            for item in info_words:
+                for word in filter_words:
+                    if word in item:
+                        other_info.append(re.sub(word,'',item)*5)
+                if "简介:" in item:
+                    other_info.append(re.sub("简介:",'',item))
+            info_dict['profile'] = "".join(other_info)
+        except :
+            raise FetchError
+        return info_dict
+
+    def get_user_fans(self,uid):
+        fans_list = []
+        page = 1
+        while page <= 15:
+            fans_page = 'http://weibo.cn/' + str(uid) + "/fans?page=" + str(page)
+            HTML_str = self.openURL(fans_page)
+            time.sleep(0.5)
+            fans = HTML_str.xpath('//td[@ valign="top"]/a')
+            #print len(follows)
+            for item in fans:
+                if item.text == '关注他':
+                    fans_uid = re.findall(r'uid=\d+',item.attrib['href'])[0]
+                    fans_uid = re.sub(r'uid=','',fans_uid)
+                    #print fans_uid
+                    fans_list.append(fans_uid)
+            page += 1
+        return fans_list
+
+
+class LoginError(Exception):
+    def __init__(self,name = "Login failed!"):
+        self.name = name
+        self.code = 1
+
+class FetchError(Exception):
+    def __init__(self):
+        self.code = 2
+
+
+
+if __name__ == '__main__':
+    username = 'kobe1993hxj@163.com'
+    passwd = 'hao123'
+    myFetcher = Fetcher(username,passwd)
+    myFetcher.login()
+    weibos = myFetcher.get_user_weibos(u'49766754')
+    for weibo in weibos:
+        print weibo
---
+++ get_user_interest.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys).setdefaultencoding('utf-8')
+from collections import Counter
+import re
+import codecs
+import time
+
+import jieba
+import jieba.analyse
+from Fetcher import *
+
+def get_keywords(myFetcher,uid_list):
+    '''
+        从uid_list的所有用户信息文本中抽取关键词
+        input:
+            myFetcher:Fetcher对象,用于爬取用户信息
+            uid_list:用户uid列表,list of unicode
+        output:
+            keywords:关键词列表[(关键词1,权重1)……]
+    '''
+    info_dict = {}
+    tags = []#用户的tags信息列表
+    profiles = []#用户的profile信息列表
+    for uid in uid_list:
+        uid = unicode(uid)
+        #print uid
+        info_dict = myFetcher.get_user_tags(uid)
+        time.sleep(0.3)
+        user_tags = info_dict['tags'] #unicode
+        user_profile = info_dict['profile']
+        #print user_tags+user_profile
+        if len(user_tags):
+            tags.append(user_tags)
+        if len(user_profile):
+            profiles.append(user_profile)
+    tags_str = "".join(tags)
+    profiles_str = "".join(profiles)
+    tags_str += profiles_str
+    jieba.analyse.set_stop_words('stop_words.txt')
+    keywords = jieba.analyse.extract_tags(tags_str,topK=30,withWeight=True)
+    return keywords
+
+def find_topic(weibos):
+    '''
+        weibos:unicode,用户所有微博拼接成的字符串
+    '''
+    topic_list = re.findall(r'#.*?#',weibos)
+    topics = [re.sub('#','',topic) for topic in topic_list]
+    counter = Counter(topics)
+    top_topics = counter.most_common()
+    with open('topics.txt','w') as fw:
+        for item in top_topics:
+            fw.write("%s\t%d\n"%(item[0],item[1]))
+
+def get_interest(uid,username,passwd):
+    '''
+        uid:用户id,unicode
+        得到uid用户所发微博中的前20高频词
+    '''
+    with codecs.open('stop_words.txt','r','utf-8') as fr:
+        stop_words = [line.strip() for line in fr]
+    stop_words.append(' ')
+    stop_words.append('\n')
+    stop_words.append('\t')
+
+    myFetcher = Fetcher(username,passwd)
+    myFetcher.login()
+
+    
+    follows_list = myFetcher.get_user_follows(uid)
+    follows_list.append(uid)
+    
+    fans_list = myFetcher.get_user_fans(uid)
+    print len(follows_list)
+    print len(fans_list)
+
+    follows_keywords = get_keywords(myFetcher,follows_list)
+    follows_interest = {}
+    for word in follows_keywords:
+            follows_interest[word[0]] = word[1]
+
+    fans_keywords = get_keywords(myFetcher,fans_list)
+    fans_interest = {}
+    for word in fans_keywords:
+            fans_interest[word[0]] = word[1]
+
+    user_weibos = myFetcher.get_user_weibos(uid)
+    weibos = ".".join(user_weibos)
+    find_topic(weibos)
+
+    content_interest = {}#从用户发布的微博信息中提取的兴趣关键词
+    words = jieba.cut(weibos)
+    filtered_words = [word for word in words if word not in stop_words and len(word) > 1]
+    all_words_count = float(len(filtered_words))
+    counter = Counter(filtered_words)
+    key_words = counter.most_common(30)
+    outputs = []
+    for item in key_words:
+        if isinstance(item[0],unicode):
+            k_word = item[0].decode('utf-8')
+            weight = float(item[1])/all_words_count
+        else:
+            k_word = item[0]
+            weight = float(item[1])/all_words_count
+        outputs.append("%s\t%f\n"%(k_word,weight))
+        content_interest[k_word] = weight
+
+    #对两类兴趣词的权重进行归一化
+    max_weight_content = max(content_interest.values())
+    max_weight_follows = max(follows_interest.values())
+    max_weight_fans = max(fans_interest.values())
+
+    for word1,weight1 in content_interest.iteritems():
+        weight1 /= max_weight_content
+
+    for word2,weight2 in follows_interest.iteritems():
+        weight2 /= max_weight_follows
+
+    for word3,weight3 in fans_interest.iteritems():
+        weight3 /= max_weight_fans
+
+    interest_words = {}
+    all_words = follows_interest.keys() + content_interest.keys() + fans_interest.keys()
+
+    for word in all_words:
+        content_weight = content_interest.get(word,0)
+        follows_weight = follows_interest.get(word,0)
+        fans_weight = fans_interest.get(word,0)
+        all_weight = 0.5*follows_weight + 0.4*content_weight + 0.1*fans_weight 
+        interest_words[word] = all_weight
+
+    sorted_words = sorted(interest_words.iteritems(),key=lambda (k,v):v,reverse=True)
+
+    for item in sorted_words[:30]:
+        print item[0].encode('utf-8',''),item[1]
+
+if __name__ == '__main__':
+    username = 'kobe1994hxj@163.com'#微博登陆账号
+    passwd = 'hao123'#微博登陆密码
+    get_interest(u'2865191107',username,passwd)
